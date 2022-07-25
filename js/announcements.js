a9a8d5b09501aed51c9cbb9e49a93c33e3715aa9
hc205 <98992299+hc205@users.noreply.github.com>
2022-07-25 16:23:20 -0500

    [CF-Pages-Skip] update

--- js/announcements.js
+++
@@ -1,5 +0,0 @@
-var myRequest = new Request('https://raw.githubusercontent.com/hc-schoolextensions/slcs-band-website/data/announcements.txt');
-
-fetch(myRequest)
-    .then(response => response.text())
-    .then(data => document.getElementById('announcements-container').textContent = data);
--- js/student-announcements.js
+++
@@ -1,5 +0,0 @@
-var myRequest = new Request('https://raw.githubusercontent.com/hc-schoolextensions/slcs-band-website/data/student-announcements.txt');
-
-fetch(myRequest)
-    .then(response => response.text())
-    .then(data => document.getElementById('st-announcements-container').innerHTML = data);
